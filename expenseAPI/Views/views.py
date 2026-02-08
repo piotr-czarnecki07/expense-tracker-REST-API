d0aaff0431784c6aefcffb89d1a2edef7a577e5b
Piotr Czarnecki <czarnecki.devv@gmail.com>
2026-02-08 17:02:08 +0100

    feat/add functionality to add users
    1. Create hash_test and dehash_text functions that encrip text based on hash_table and dehash_table
    
    2. Add POST method functionality to '/users/' view that adds a new user to database. Generates a JWT for this user. Token and password are hashed before being added to the db.

--- expenseAPI/Views/views.py
+++ expenseAPI/Views/views.py
@@ -8,6 +8,21 @@ from django.db import DatabaseError
 from DB.models import User
 from Views.serializers import UserSerializer
 
+from Views.hash import hash_table, dehash_table, CHARACTERS
+import random
+
+def hash_text(text: str) -> str:
+    hashed = ''
+    for c in text:
+        hashed += hash_table[c]
+    return hashed
+
+def dehash_text(text: str) -> str:
+    dehashed = ''
+    for c in text:
+        dehashed += dehash_table[c]
+    return dehashed
+
 @api_view(['GET', 'POST'])
 def all_users(request):
     if request.method == 'GET': # return a list of all users
@@ -21,11 +36,42 @@ def all_users(request):
         else:
             return Response(serializer.data, st.HTTP_200_OK)
 
-    elif request.method == 'POST': # add a new user
-        pass
+    else: # POST method; add a new user
+        try:
+            username = request.data['username']
+            email = request.data['email']
+            password = request.data['password']
 
-    else:
-        return Response({'error': 'Invalid HTTP method'}, st.HTTP_400_BAD_REQUEST)
+        except KeyError as e:
+            return Response({'error': f'Parameter {e} was not provided'}, st.HTTP_400_BAD_REQUEST)
+
+        # parameters validation
+        if User.objects.filter(username=username).first() is not None:
+            return Response({'error': 'This username already exists'}, st.HTTP_409_CONFLICT)
+
+        if User.objects.filter(email=email).first() is not None:
+            return Response({'error': 'This email is already in use'}, st.HTTP_409_CONFLICT)
+
+        if '@' not in email or '.' not in email:
+            return Response({'error': 'Email is invalid'}, st.HTTP_400_BAD_REQUEST)
+
+        token = ''
+        for _ in range(50):
+            token += CHARACTERS[random.randint(0, 43)]
+
+        try:
+            user = User(username=username, email=email, password=hash_text(password), token=hash_text(token))
+            user.save()
+
+        except ValidationError as e:
+            return Response({'error': f'User data is invalid, {e}'}, st.HTTP_400_BAD_REQUEST)
+        
+        except DatabaseError as e:
+            return Response({'error': f'Database is not available: {e}'}, st.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        else:
+            serializer = UserSerializer(user)
+            return Response(serializer.data, st.HTTP_201_CREATED)
 
 @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
 def specific_user(request, user: int):
